47a132384ea2b2eaab1b6fe80193d262889e841f
cmhack <45024982+cmhack@users.noreply.github.com>
2020-06-14 22:53:09 +0800

    Update HomeCtrl.js

--- js/controller/HomeCtrl.js
+++ js/controller/HomeCtrl.js
@@ -4,7 +4,7 @@ app.controller('HomeCtrl', function($scope){
 	
 	$scope.soundON = true;
 	
-	var audio = new Audio('../audio/Fast Track.mp3');
+	var audio = new Audio('audio/Fast Track.mp3');
 	audio.play();
 	
 	$scope.playAudio = function(param){
