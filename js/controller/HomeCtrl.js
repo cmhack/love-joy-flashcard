fc82674f1ded0180c952e9c5573fcfb27d2c187b
MrConan <conanchanmh@hotmail.com>
2020-06-15 21:07:53 +0800

    Major update for home login

--- js/controller/HomeCtrl.js
+++ js/controller/HomeCtrl.js
@@ -17,4 +17,27 @@ app.controller('HomeCtrl', function($scope){
 		}
 	}
 
-});
+	var attempt = 3;
+
+	$scope.validate = function(){
+		var login = document.getElementById("login").value;
+		var password = document.getElementById("password").value;
+
+		if (login == "NJ001" && password == "312127"){
+			// alert("Login successfully");
+			window.location = "flashCard.html";
+			return false;
+		} 
+		else{
+			attempt --;
+			// alert("Wrong Login id or password!")
+			alert("Wrong Login id or password! \n \n You still have left "+attempt+" attempt.");
+			if(attempt == 0){
+				document.getElementById("login").disabled = true;
+				document.getElementById("password").disabled = true;
+				document.getElementById("submit").disabled = true;
+				return false;
+				}
+			}
+		}
+	});
